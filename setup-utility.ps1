ac13ed991aadaaa2c7d117161ab444abb9286584
TupiNUMBooR@Wolf <tupinumboor@gmail.com>
2025-09-04 15:20:03 +0300

    Update windows installer

--- setup-utility.ps1
+++ setup-utility.ps1
@@ -10,7 +10,8 @@ New-Item -ItemType Directory -Force -Path $tools | Out-Null
 # Копируем run.bat → utility.bat
 Copy-Item -Path ".\run.bat" -Destination "$tools\utility.bat" -Force
 Copy-Item -Path ".\run.sh" -Destination "$tools\utility" -Force
-Write-Host "Copied as: $tools\utility.bat and $tools\utility"
+Copy-Item -Path ".\run-git-bash.sh" -Destination "$tools\utility-git-bash" -Force
+Write-Host "Copied to $tools"
 
 # Добавляем в PATH пользователя
 [Environment]::SetEnvironmentVariable("Path", "$env:Path;$tools", "User")
